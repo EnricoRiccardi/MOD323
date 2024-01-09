43ab6ec8c2f2c6864bfbd2bb72d59a5a33b46800
EnricoRiccardi <e.riccardi82@gmail.com>
2024-01-09 20:26:06 +0100

    slider fixed

--- src/slides/clean.sh
+++ src/slides/clean.sh
@@ -3,5 +3,6 @@ cp ../scripts.py .
 python -c 'import scripts; scripts.clean()'
 rm -rf scripts.py
 rm -rf runestone sphinx-* *.pyc automake*
-rm -rf _build __pycache__
+rm -rf _build __pycache__ _mint*
 rm -rf *.aux *.dlog *.html *.idx *.log *.out tmp_* *.old* *.tex
+rm -rf *.nav *.pyg *.snm *.vrb *.toc *.sty
